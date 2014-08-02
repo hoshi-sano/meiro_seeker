5f4c0355738586d54037cd38cf260a32d3cdbc8e
hoshi-sano <hoshi.sanou@gmail.com>
2014-08-02 17:55:56 +0900

    盾アニメーションを仮で追加。

--- lib/model/floor_object/item/iron_shield.rb
+++ lib/model/floor_object/item/iron_shield.rb
@@ -5,8 +5,7 @@ module MyDungeonGame
     name MessageManager.get('dict.items.iron_shield.name')
     note MessageManager.get('dict.items.iron_shield.note')
     image IMAGES[:weapon] # TODO: 盾画像を使う
-    # equipped_image_pathは暫定
-    equipped_image_path File.join(ROOT, 'data', 'weapon_effect.png')
+    equipped_image_path File.join(ROOT, 'data', 'shield_effect.png')
     base_strength 5
   end
 end
