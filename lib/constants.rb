98939b4ddad4dcaa2a9f6b0eeafbe7dc0d1df6ac
hoshi-sano <hoshi.sanou@gmail.com>
2014-08-04 09:29:22 +0900

    コミット漏れ

--- lib/constants.rb
+++ lib/constants.rb
@@ -138,6 +138,7 @@ module MyDungeonGame
   STAIRS_IMAGE_PATH = File.join(ROOT, 'data', 'stairs.png')
   WEAPON_IMAGE_PATH = File.join(ROOT, 'data', 'weapon.png')
   SHIELD_IMAGE_PATH = File.join(ROOT, 'data', 'shield.png')
+  BULLET_IMAGE_PATH = File.join(ROOT, 'data', 'shuriken.png')
   POTION_IMAGE_PATH = File.join(ROOT, 'data', 'potion.png')
   MANJU_IMAGE_PATH  = File.join(ROOT, 'data', 'manju.png')
   WORD_LIST_PATH    = File.join(ROOT, 'data', 'words.yml')
