fbc52fb12969e443dfc083b4c98dc2da3b6bc430
hoshi-sano <hoshi.sanou@gmail.com>
2016-01-05 08:44:45 +0900

    部屋の出入り口にいても敵がアイテム投擲をするよう修正

--- lib/model/room.rb
+++ lib/model/room.rb
@@ -7,9 +7,16 @@ module MyDungeonGame
 
     # 部屋内にプレイヤーがいる場合、その座標を返す。
     # 部屋内にプレイヤーがいない場合はnilを返す。
+    # 部屋の出入り口も部屋内に含む。
     def player_xy
       res = nil
       each_coordinate do |x, y|
+        if chara = floor[x, y].character
+          res = [x, y] if chara.type == :player
+        end
+      end
+      return res if res
+      gate_coordinates.each do |x, y|
         if chara = floor[x, y].character
           res = [x, y] if chara.type == :player
         end
