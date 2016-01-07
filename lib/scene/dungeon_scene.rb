02d380be4da507aa4bcb60c01e28a2171ef38e2f
hoshi-sano <hoshi.sanou@gmail.com>
2016-01-07 09:38:50 +0900

    出現アイテムの種類と出現率をyamlで指定できるよう修正

--- lib/scene/dungeon_scene.rb
+++ lib/scene/dungeon_scene.rb
@@ -38,37 +38,54 @@ module MyDungeonGame
       res
     end
 
+    # 設定ファイルで指定されるアイテムと出現率をもとに、
+    # ランダムアイテムテーブルを作成する
+    # NOTE: 出現する階層も指定できた方がよいかもしれない
+    # TODO: 罠にも対応する
+    def create_floor_object_table
+      return nil unless @map_info[:item_table]
+      @item_table = {}
+      rate_counter = 0
+      @map_info[:item_table].each do |k, v|
+        item_klass = MyDungeonGame.const_get(k)
+        @item_table[item_klass] = (rate_counter...(rate_counter + v))
+        rate_counter += v
+      end
+      @item_table
+    end
+
+    # 出現率に基づいてアイテムテーブルからアイテムをランダムで返す
+    def item_random_select
+      return nil unless @item_table
+      _, last_item_range = @item_table.to_a.last
+      max_counter = last_item_range.last
+      v = DungeonManager.randomizer.rand(max_counter)
+      res = nil
+      @item_table.each do |item_klass, range|
+        if range.include?(v)
+          res = item_klass.new
+          break
+        end
+      end
+      res
+    end
+
     def create_floor_objects(storey)
       res = []
-      # TODO: 階に合わせた適切なフロアオブジェクト選択を行う
-      items = [
-               Kizugusuri,
-               KaifukuNoKusuri,
-               ChikaraNoKusuri,
-               ConfusionPotion,
-               NikuMan,
-               Sabel,
-               GhostBuster,
-               IronShield,
-               HungryShield,
-               AntiHungryShield,
-               LightRing,
-               CalmRing,
-               NormalBullet,
-               ThunderGrimoire,
-               LightGrimoire,
-               ConfusionGrimoire,
-               WeaponEnhancementGrimoire,
-               ShieldEnhancementGrimoire,
-              ]
-      5.times do
-        item = items[DungeonManager.randomizer.rand(items.size)].new
-        set_random_position(item)
-        res << item
+      create_floor_object_table
+      if @item_table
+        # 1フロアのアイテム数は3~5個
+        item_num = DungeonManager.randomizer.rand(3) + 3
+        item_num.times do
+          item = item_random_select
+          set_random_position(item)
+          res << item
+        end
       end
       stairs = Stairs.new(@floor)
       set_random_position(stairs)
       res << stairs
+      # TODO: 罠の設置
       res
     end
 
